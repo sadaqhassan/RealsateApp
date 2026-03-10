d8803a0684cd1681a1c7cb0eb086336d764935d7
sadaqsam <sadaqsamor99@gmail.com>
2026-03-10 11:21:12 +0300

    createdCard

--- client/src/Components/Resents.jsx
+++ client/src/Components/Resents.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import SellCard from './SellCard'
 
 const Resents = () => {
   return (
-    <div>Resents</div>
+    <div className='mt-10 mb-5'>
+      {/* //offers */}
+      <div className='mt-10'>
+        <h1 className='text-start my-5 text-2xl md:text-3xl font-medium'>
+          Recent offers
+        </h1>
+        <SellCard/>
+      </div>
+
+
+    </div>
   )
 }
 
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Banner from '../Components/Banner'
+import Resents from '../Components/Resents'
 
 const Home = () => {
   return (
     <div className='mt-20'>
       <Banner/>
+      <hr className='mt-4 mb-4 text-cyan-700' />
+      <Resents/>
     </div>
   )
 }
