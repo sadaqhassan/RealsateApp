e6c9fe6b5f7b3c089ca497bb7297d09c297ac187
sadaqsam <sadaqsamor99@gmail.com>
2026-03-10 09:50:00 +0300

    some Fixing

--- client/src/Components/Banner.jsx
+++ client/src/Components/Banner.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Login from '../Pages/Login'
+import { ArrowBigRight } from 'lucide-react'
 
 const Banner = () => {
   return (
     <div className='flex justify-between mx-20 flex-wrap items-center'>
-      <h1 className='text-start text-2xl md:text-5xl text-gray-700 font-bold'>Find your next perfect <br/>place with ease</h1>
+      <div>
+      <h1 className='text-start text-2xl md:text-5xl text-gray-700 font-bold mb-4'>Find your next perfect <br/>place with ease</h1>
+      <button className='m-4 flex gap-2 mt-4 bg-cyan-600 p-2 text-white rounded items-center'>Get started <ArrowBigRight/></button>
+      </div>
       <Login/>
     </div>
   )
