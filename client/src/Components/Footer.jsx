f704b44337a0b624e0a0291ee1b6774e6357beb2
sadaqsam <sadaqsamor99@gmail.com>
2026-03-10 13:39:58 +0300

    someFix

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -13,7 +13,7 @@ function Footer() {
             `}</style>
             
             <footer className="flex flex-col items-center justify-around w-full py-16 text-sm bg-slate-50 text-gray-800/70">
-               <p className='text-3xl font-bold text-cyan-600'> Samo RealState</p>
+            <p className='text-3xl font-bold text-cyan-600'> Samo RealState</p>
                 <p className="mt-4 text-center">Copyright © 2025 <a href="https://prebuiltui.com">Samo Real State</a>. All rights reservered.</p>
                 <div className="flex items-center gap-4 mt-6">
                     <a href="#" className="font-medium text-gray-800 hover:text-black transition-all">
@@ -27,7 +27,7 @@ function Footer() {
             </footer>
         </>
     </div>
-  )
+)
 }
 
 export default Footer
