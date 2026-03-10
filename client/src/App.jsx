e42c79db57638dba5598ef4fe971a7260d8bb290
sadaqsam <sadaqsamor99@gmail.com>
2026-03-10 13:51:33 +0300

    path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import Footer from './Components/Footer'
 import About from './Pages/About'
+import Login from './Pages/Login'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
       </div>
       <Footer/>
