446d69359fa0fe430e8ad384ee3c55441f634f9d
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-11 10:01:27 +0300

    wrped to the server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import { configApp } from './configs/config.js';
 import { mongodbConn } from './configs/conn.js';
+import { userRoute } from './Routes/user.route.js';
 const app = express();
 
 configApp(app);
 
 mongodbConn();
+
+app.use("/api/user",userRoute);
 
