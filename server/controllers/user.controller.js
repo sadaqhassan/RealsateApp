94447f914a270cf074af52650f860f75cfc92891
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-11 09:47:28 +0300

    RegisterApi

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,9 +1,23 @@
-export const RegiserApi = async()=>{
-    const {email,password,name} = req.body
+//registerApi
 
+import { USER } from "../Model/user.model.js"
+
+export const RegiserApi = async(req,res)=>{
+    const {email,password,username} = req.body
+
+    if(!email || !password || !username){
+        return res.status(400).json({success:false,message:"plese fill all fields"})
+    }
     try {
-        
+        const isExist = await USER.findOne({email});
+        if(isExist){
+            return res.status(400).json({success:false,message:"this user is exist"})
+        }
+
+
     } catch (error) {
-        
+        res.status(500).json({success:false,message:"server error"})
     }
 }
+
+
