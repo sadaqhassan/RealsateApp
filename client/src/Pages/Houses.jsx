cc5f4c21db68741dd7d7e3d840020e7ff6a9253c
sadaqhassan <abdallaabdurahman845@users.noreply.github.com>
2026-03-11 05:41:41 +0300

    Added all houses pages

---
+++ client/src/Pages/Houses.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Houses = () => {
+  return (
+    <div className='mt-10'>
+        <div className='flex flex-col space-y-4'>
+            <h1 className='text-2xl md:text-3xl font-medium'>All Houses</h1>
+            <div className='bg-green-500 w-20 h-1'></div>
+        </div>
+    </div>
+  )
+}
+
+export default Houses
