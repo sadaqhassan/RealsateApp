403ca1cc9c6b94005254ecbd6b6ed8b1de8b7f34
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-11 09:51:36 +0300

    LoginApi

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,8 @@
-//registerApi
-
 import { USER } from "../Model/user.model.js"
+import bcrypt from "bcryptjs"
+
+
+//registerApi
 
 export const RegiserApi = async(req,res)=>{
     const {email,password,username} = req.body
@@ -14,6 +16,17 @@ export const RegiserApi = async(req,res)=>{
             return res.status(400).json({success:false,message:"this user is exist"})
         }
 
+        const hashedPassword = bcrypt.hashSync(password,10);
+
+        const newUser = new USER({
+            username,
+            email,
+            password:hashedPassword
+        })
+
+        await newUser.save();
+
+        return res.status(201).json({success:true,message:"registeration successfully"})
 
     } catch (error) {
         res.status(500).json({success:false,message:"server error"})
@@ -21,3 +34,24 @@ export const RegiserApi = async(req,res)=>{
 }
 
 
+
+export const LoginApi = async(req,res)=>{
+    const {email,password,username} = req.body
+
+    if(!email || !password || !username){
+        return res.status(400).json({success:false,message:"plese fill all fields"})
+    }
+    try {
+        const isExist = await USER.findOne({email});
+        if(!isExist){
+            return res.status(400).json({success:false,message:"this user is exist"})
+        }
+
+        const hashedPassword = bcrypt.hashSync(password,10);
+
+        return res.status(400).json({success:false,message:"plese fill all fields"})
+
+    } catch (error) {
+        res.status(500).json({success:false,message:"server error"})
+    }
+}
