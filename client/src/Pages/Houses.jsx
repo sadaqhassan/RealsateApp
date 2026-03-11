46093855cc15aea98ceb70380966eef2aef8916d
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-11 06:08:37 +0300

    Added products

--- client/src/Pages/Houses.jsx
+++ client/src/Pages/Houses.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import { demoResentOffersData } from '../helper.js'
+import SellCard from '../Components/SellCard.jsx'
 
 const Houses = () => {
   return (
     <div className='mt-10'>
-        <div className='flex flex-col space-y-4'>
+        <div className='flex flex-col'>
             <h1 className='text-2xl md:text-3xl font-medium'>Availible Houses</h1>
             <div className='bg-green-500 w-20 h-1'></div>
         </div>
+
+        <div className='md:grid flex grid-cols-2 mt-10 mb-5  flex-wrap md:grid-cols-4 md:space-x-0 space-y-4  space-x-5'>
+        {
+          demoResentOffersData.map((home,index)=>(
+            <SellCard home={home} key={index}/>
+          ))
+        } 
+      </div>
     </div>
   )
 }
