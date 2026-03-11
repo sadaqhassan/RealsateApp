b67a60337f07690d2f4f33ff6a3d740997ba99c0
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-11 06:11:21 +0300

    added hdr

--- client/src/Pages/BuyingCenter.jsx
+++ client/src/Pages/BuyingCenter.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const BuyingCenter = () => {
   return (
-    <div>BuyingCenter</div>
+    <div className='mt-10'>
+        <div className='flex flex-col'>
+            <h1 className='text-2xl md:text-3xl font-medium'>Available Houses</h1>
+            <div className='bg-green-500 w-20 h-1'></div>
+        </div>
+    </div>
   )
 }
 
