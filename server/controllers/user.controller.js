2fb77aaa9d919e4ef6e6e45f151f8e5a7cccd6c1
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-12 19:05:32 +0300

    conttllr

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,6 @@
 import { USER } from "../Model/user.model.js"
 import bcrypt from "bcryptjs"
 import jwt from 'jsonwebtoken'
-import { trying } from "../utils/helper.js"
 
 //registerApi
 
@@ -39,32 +38,46 @@ export const RegisterApi = async(req,res)=>{
 export const LoginApi = async(req,res)=>{
     const {email,password} = req.body
 
-    trying(email);
-    
-    if(!email || !password){
+    if(!email || !password ){
         return res.status(400).json({success:false,message:"plese fill all fields"})
     }
     try {
+
         const isExist = await USER.findOne({email});
+
         if(!isExist){
             return res.status(400).json({success:false,message:"incorrect email or password"})
         }
 
+
+        if(isExist.lockedUntill && isExist.lockedUntill > Date.now()){
+            let time = new Date(isExist.lockedUntill).toLocaleTimeString()
+            return res.status(400).json({success:false,message:"Sorry try gain after ."+time});
+        }
+
         const isCompare = bcrypt.compareSync(password,isExist.password);
 
+        
         if(!isCompare){
+            isExist.atempts +=1
+            if(isExist.atempts >= 5){
+                isExist.lockedUntill = Date.now()+1*60*1000
+            }
+            await isExist.save()
             return res.status(400).json({success:false,message:"incorrect email or password"})
         }
 
-        const token = jwt.sign({id:isExist._id},process.env.JWT_SECRET,{expiresIn:"1d"})
+        isExist.lockedUntill = undefined
+        isExist.atempts = 0
 
+        const token = jwt.sign({id:isExist._id},process.env.JWT_SECRET,{expiresIn:"1d"})
         res.cookie("token",token,{
             httpOnly: true,
-        });
-
+        })
         return res.status(200).json({success:true,message:"login successfully"})
 
     } catch (error) {
+        console.log(error)
         res.status(500).json({success:false,message:"server error"})
     }
 }
