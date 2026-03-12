08348fb5ebb78b9d25505a572702ccfcd4cf8a3c
sadaqhassan <abdallaabdurahman845@gmail.com>
2026-03-12 05:45:05 +0300

    added cors

--- server/configs/config.js
+++ server/configs/config.js
@@ -1,9 +1,11 @@
 import cookieParser from "cookie-parser"
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 export const configApp = (app)=>{
     dotenv.config()
+    app.use(cors())
     app.use(express.json())
     app.use(cookieParser())
     app.listen(process.env.PORT,()=>console.log("server is running on"+process.env.PORT))
